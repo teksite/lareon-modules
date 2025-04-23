43134a88f95681bb8623b4b6cedc5413157fc789
teksite <teksite.design@gmail.com>
2025-04-23 15:45:50 +0330

    change trait name: Hastag to taggable

--- App/Traits/Taggable.php
+++ App/Traits/Taggable.php
@@ -5,7 +5,7 @@ namespace Lareon\Modules\Tag\App\Traits;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Lareon\Modules\Tag\App\Models\Tag;
 
-trait HasTag
+trait Taggable
 {
     use SaveTag;
 
